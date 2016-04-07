12d786fad73e228005d712faa9279391c726c0fb
Nick Sabalausky <Abscissa@users.noreply.github.com>
2016-04-07 17:07:56 -0400

    Caching from prev commit didn't work. Don't know why. [ci skip]

--- install-deps.sh
+++ install-deps.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 DMD_ZIP=dmd.2.070.2.linux.zip
-
-if [ ! -d "local-dmd" ]; then
-	wget http://downloads.dlang.org/releases/2016/$DMD_ZIP
-	unzip -d local-dmd $DMD_ZIP
-fi
+wget http://downloads.dlang.org/releases/2016/$DMD_ZIP
+unzip -d local-dmd $DMD_ZIP
