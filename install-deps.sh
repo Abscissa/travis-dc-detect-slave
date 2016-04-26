46646fac5fcfbd00333ffcdaae7f1f7b5a39a9a9
Nick Sabalausky <Abscissa@users.noreply.github.com>
2016-04-25 23:04:54 -0400

    install-deps: Set reporter-command to post data to official travis-dc-detect-master server. Should now work.

--- install-deps.sh
+++ install-deps.sh
@@ -2,4 +2,6 @@
 DMD_ZIP=dmd.2.070.2.linux.zip
 wget http://downloads.dlang.org/releases/2016/$DMD_ZIP
 unzip -d local-dmd $DMD_ZIP
-cp --no-clobber config.example.sdl config.sdl
+
+echo "// See config.example.sdl for documentation for this file" > config.sdl
+echo "reporter-command \"cd reporter/postToHTTPS && dub run --compiler=../../local-dmd/dmd2/linux/bin64/dmd -- https://semitwist.com/travis-d-compilers/compiler\"" >> config.sdl
